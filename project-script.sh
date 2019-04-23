8f4154d1b3227bf6c9171cbbfb6fcfc90848eb49
bhavanashendge <47981943+bhavanashendge@users.noreply.github.com>
2019-04-23 08:25:31 -0400

    Create project-script.sh

---
+++ project-script.sh
@@ -0,0 +1,6 @@
+ssh $1 'wget -P /tmp https://apt.puppetlabs.com/puppet6-release-bionic.deb'
+ssh $1  'sudo dpkg -i /tmp/puppet6-release-bionic.deb'
+ssh $1  'sudo apt-get update'
+ssh $1  'sudo apt-get install puppet-agent'
+ssh $1  'echo ''$2 puppet'' | sudo tee -a /etc/hosts'
+ssh $1  '/opt/puppetlabs/bin/puppet agent --waitforcert 20 --test &'
