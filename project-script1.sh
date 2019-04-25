f8f44eb319b4e81c0d247c31f6971fb7c6c80af4
bhavanashendge <47981943+bhavanashendge@users.noreply.github.com>
2019-04-25 08:46:40 -0400

    Update project-script1.sh

--- project-script1.sh
+++ project-script1.sh
@@ -7,6 +7,6 @@ ssh $1  'sudo apt-get update'
 echo "Installing puppet-agent"
 ssh $1  'sudo apt-get install puppet-agent'
 echo "Adding host file entry as $2 puppet"
-ssh $1  "echo $2 puppet >> /etc/hosts"
+ssh $1  'echo ''192.168.1.200 puppet'' | sudo tee -a /etc/hosts'
 echo "Running puppet agent with --test"
 ssh $1  '/opt/puppetlabs/bin/puppet agent --waitforcert 20 --test &'
